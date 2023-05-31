0d16df0c51e4b7505466109aa3178d0f46d1e28c
mariusrundereim <marius.rundereim@gmail.com>
2023-05-31 14:15:58 +0200

    first upload

---
+++ js/portfolio.js
@@ -0,0 +1,13 @@
+const studyProjects = [
+  {
+    title: "Rainy Days",
+    assignment: "Course Assignment",
+    year: 2022,
+    desc: "Lorem ipsum dolor",
+    cover: "coverimage.png",
+    src: ["image-1", "image-2", "image-3", "image-4"],
+    url: "www.netlify.com",
+  },
+];
+
+console.log(studyProjects[0].url);
