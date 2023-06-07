98033ff815ba781561026ffe40bf2de3e1651460
mariusrundereim <marius.rundereim@gmail.com>
2023-06-07 12:29:43 +0200

    update portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,34 +1,39 @@
 const studyProjects = [
   {
+    title: "StyleVault",
+    assignment: "Exam 1",
+    year: 2023,
+    desc: "Lorem ipsum dolor",
+    cover: "./images/stylevault_cover.png",
+    url: [
+      "https://earnest-lamington-1404cb.netlify.app/",
+      "https://github.com/mariusrundereim/rundereim_marius_exam1",
+    ],
+  },
+  {
+    title: "Community Science Museum",
+    assignment: "Semester Exam 1",
+    year: 2022,
+    desc: "Lorem ipsum dolor",
+    cover: "./images/communityscience_cover.png",
+    url: [
+      "https://capable-crepe-8d1091.netlify.app/",
+      "https://github.com/mariusrundereim/semester-museum-1",
+    ],
+  },
+  {
     title: "Rainy Days",
     assignment: "Course Assignment",
     year: 2022,
     desc: "Lorem ipsum dolor",
-    cover: "coverimage.png",
-    src: [
-      {
-        image: "image-1.png",
-        alt: "Image text description",
-      },
-      {
-        image: "image-2.png",
-        alt: "Image text description",
-      },
-      {
-        image: "image-3.png",
-        alt: "Image text description",
-      },
-      {
-        image: "image-4.png",
-        alt: "Image text description",
-      },
+    cover: "./images/rainydays_cover.png",
+    url: [
+      "https://stunning-sawine-0a97a9.netlify.app/",
+      "https://github.com/mariusrundereim/rainydays-cms",
     ],
-    url: "www.netlify.com",
   },
 ];
 
 //console.log(studyProjects[0].src[1].image);
 
-studyProjects[0].src.forEach((item) => {
-  console.log(item.image);
-});
+console.log(studyProjects[0].cover);
